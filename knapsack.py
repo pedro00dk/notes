cd29ce3d9c466987b910c4e2eb7d85a60fe0158b
Pedro Henrique <pedro00dk@gmail.com>
2020-05-04 15:49:31 -0300

    Add multiple implementations of simple knapsack

---
+++ knapsack.py
@@ -0,0 +1,41 @@
+def knapsack_naive(items, capacity, item=0):
+    if item == len(items) or capacity == 0:
+        return 0
+    value, weight = items[item]
+    return max(
+        knapsack_naive(items, capacity, item + 1),
+        0 if capacity < weight else value + knapsack_naive(items, capacity - weight, item + 1)
+    )
+
+
+def knapsack_dynamic(items, capacity):
+    k = [[0 for x in range(capacity + 1)] for x in range(len(items)+1)]
+    for i in range(1, len(items) + 1):
+        value, weight = items[i - 1] if i > 0 else (0, 0)
+        for c in range(1, capacity + 1):
+            k[i][c] = 0 if i == 0 or c == 0 else max(k[i - 1][c], 0 if c < weight else value + k[i - 1][c - weight])
+    return k[-1][-1]
+
+
+def knapsack_memo(items, capacity, item=0, memo={}):
+    if item == len(items) or capacity == 0:
+        return 0
+    if memo.setdefault(item, {}).setdefault(capacity, 0) == 0:
+        value, weight = items[item]
+        memo[item][capacity] = max(
+            knapsack_memo(items, capacity, item + 1, memo),
+            0 if capacity < weight else value + knapsack_memo(items, capacity - weight, item + 1, memo)
+        )
+    return memo[item][capacity]
+
+
+def test():
+    items = [(60, 10), (100, 20), (120, 30)]
+    capacity = 50
+    print(knapsack_naive(items, capacity))
+    print(knapsack_dynamic(items, capacity))
+    print(knapsack_memo(items, capacity))
+
+
+if __name__ == '__main__':
+    test()
