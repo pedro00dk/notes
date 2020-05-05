ad977aa955fdb470713de0c4592f45659810cbe0
Pedro Henrique <pedro00dk@gmail.com>
2020-05-05 07:53:14 -0300

    Better disjoint set compression

--- disjoint_set.py
+++ disjoint_set.py
@@ -5,6 +5,8 @@ class DisjointSet:
     def __init__(self, size=0):
         self.sets = [i for i in range(size)]
         self.ranks = [0 for i in range(size)]
+        self.sizes = [1 for i in range(size)]
+        self.count = len(sets)
 
     def __str__(self):
         lines = '\n'.join([f'{i} -> {self.sets[i]} # {self.ranks[i]}' for i in range(len(self.sets))])
@@ -14,16 +16,19 @@ class DisjointSet:
         key = len(self.sets)
         self.sets.append(key)
         self.ranks.append(0)
+        self.sizes.append(1)
+        self.count += 1
         return key
 
-    def find(self, key, compress=True):
+    def find(self, key):
         if key < 0 or key >= len(self.sets):
             raise KeyError('not found')
+        root = key
+        while root != self.sets[root]:
+            root = self.sets[key]
         while key != self.sets[key]:
-            if compress:
-                self.sets[key] = self.sets[self.sets[key]]
-            key = self.sets[key]
-        return key
+            key, self.sets[key] = self.sets[key], root
+        return root
 
     def union(self, key_a, key_b):
         key_a = self.find(key_a)
@@ -33,15 +38,13 @@ class DisjointSet:
         if self.ranks[key_a] < self.ranks[key_b]:
             key_a, key_b = key_b, key_a
         self.sets[key_b] = key_a
-        if self.ranks[key_a] == self.ranks[key_b]:
-            self.ranks[key_a] += 1
+        self.ranks[key_a] += 1 if self.ranks[key_a] == self.ranks[key_b] else 0
+        self.sizes[key_a] += self.sizes[key_b]
+        self.count -= 1
         return key_a
 
-    def compress(self):
-        for i in range(len(self.sets)):
-            self.find(i)
-            self.ranks[i] = 0
-
+    def connected(self, key_a, key_b):
+        return self.find(key_a) == self.find(key_b)
 
 
 class HashDisjointSet:
@@ -59,14 +62,14 @@ class HashDisjointSet:
             self.key_map.put(key, self.disjoint_set.make_set())
         return key
 
-    def find(self, key, compress=True):
-        return self.disjoint_set.find(self.key_map.get(key), compress)
+    def find(self, key):
+        return self.disjoint_set.find(self.key_map.get(key))
 
     def union(self, key_a, key_b):
         return self.disjoint_set.union(self.key_map.get(key_a), self.key_map.get(key_b))
-    
-    def compress(self):
-        self.disjoint_set.compress()
+
+    def connected(self, key_a, key_b):
+        return self.disjoint_set.connected(self.key_map.get(key_a), self.key_map.get(key_b))
 
 
 def test():
@@ -90,8 +93,6 @@ def test():
     d.union('2', '3')
     d.union('4', '0')
     d.union('1', '2')
-    print(d)
-    d.compress()
     print(d)
 
 
