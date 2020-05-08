2f51561c7b655a17a89d5e5667d1101051ff1fd7
Pedro Henrique <pedro00dk@gmail.com>
2020-05-08 19:14:18 -0300

    Some renames in topsort algorithm

--- dsa/graph/topsort.py
+++ dsa/graph/topsort.py
@@ -1,13 +1,13 @@
 from .graph import Graph
 
 
-def topsort(graph: Graph):
+def topological_sort(graph):
     order = []
     visited = [False] * len(graph)
-    for vertex in range(len(graph)):
-        if visited[vertex]:
+    for source in range(len(graph)):
+        if visited[source]:
             continue
-        for traverse_vertex, previous, *_ in graph.traverse(vertex, visited=visited, before=False):
+        for traverse_vertex, previous, *_ in graph.traverse(source, visited=visited, before=False):
             order.append(traverse_vertex)
     order.reverse()
     return order
@@ -17,7 +17,7 @@ def test():
     for i in range(10):
         g = Graph.random_dag()
         print(g)
-        print(topsort(g))
+        print(topological_sort(g))
         print('\n')
 
 
