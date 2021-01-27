6d455200e9b4094fb24f694ab62081a7682e4eed
Pedro Henrique <pedro00dk@gmail.com>
2021-01-27 18:49:08 -0300

    Move prober utilities in map.py to the end of the file

--- src/map/abc.py
+++ src/map/abc.py
@@ -6,61 +6,6 @@ K = TypeVar('K')
 V = TypeVar('V')
 
 
-@dataclasses.dataclass
-class Prober:
-    """
-    Hash probing strategies for map implemantations based on hashtables.
-
-    > parameters
-    - `load`: load threshold for the hashtable for the probe function
-    - `capacity`: a function to generate capacities for rebuilds, the first capacity and index must be 0
-    - `probe`: is a function that creates indices from a hash and number of tries to place entries in the hashtable
-    """
-    load: float
-    capacity: Callable[[int, int], int]
-    probe: Callable[[int, int, int], int]
-
-
-# prime numbers near to the 2**powers for power range of [1, 32]
-PRIMES = (
-    2, 3, 7, 17, 31, 67, 131, 257, 509, 1021, 2053, 4093, 8191, 16381, 32771, 65537, 131071, 262147, 524287, 1048573,
-    2097143, 4194301, 8388617, 16777213, 33554467, 67108859, 134217757, 268435459, 536870909, 1073741827, 2147483647,
-    4294967291
-
-
-)
-
-# Linear probing using prime size capacities.
-# Probing constants: probe: c1 = 1
-# For capacities above 2^32 it uses odd values based on the last prime.
-LINEAR_PROBER = Prober(
-    0.65,
-    lambda capacity, index: PRIMES[index + 3] if index + 3 < len(PRIMES) else capacity * 2 - 1,
-    lambda capacity, hash_, trie: (hash_ + trie) % capacity,
-)
-
-# Quadratic probing using prime size capacities.
-# However, due to probe the function, only half the indices are accessible.
-# Probing constants probe: c1 = c2 = 1; probe2: c1 = 0, c2 = 1; probe3: c1 = c2 = 1/2
-# For capacities above 2^32 it uses odd values based on the last prime (may fail).
-QUADRATIC_PRIME_PROBER = Prober(
-    0.5,
-    lambda capacity, index: PRIMES[index + 3] if index + 3 < len(PRIMES) else capacity * 2 - 1,
-    lambda capacity, hash_, trie: (hash_ + trie**2 + trie) % capacity,
-)
-# lambda capacity, hash_, trie: (hash_ + trie**2) % capacity,
-# lambda capacity, hash_, trie: (hash_ + (trie * 0.5)**2 + (trie * 0.5)) % capacity
-
-# Quadratic probing using triangular numbers that fully covers indices on a hashtable of capacity 2**n.
-# Probing constants for triangular numbers probe: c1 = c2 = 1/2
-# However, because of the even capacity, it may increase collisions among keys even and odd keys independently.
-QUADRATIC_TRIANGULAR_PROBER = Prober(
-    0.75,
-    lambda capacity, index: 2**(index + 3),
-    lambda capacity, hash_, trie: (hash_ + (trie**2 + trie) // 2) % capacity,
-)
-
-
 class Map(Generic[K, V], abc.ABC):
     """
     Abstract base class for mapping data structures.
@@ -195,3 +140,58 @@ class Map(Generic[K, V], abc.ABC):
             if value is v or value == v:
                 return True
         return False
+
+
+@dataclasses.dataclass
+class Prober:
+    """
+    Hash probing strategies for map implemantations based on hashtables.
+
+    > parameters
+    - `load`: load threshold for the hashtable for the probe function
+    - `capacity`: a function to generate capacities for rebuilds, the first capacity and index must be 0
+    - `probe`: is a function that creates indices from a hash and number of tries to place entries in the hashtable
+    """
+    load: float
+    capacity: Callable[[int, int], int]
+    probe: Callable[[int, int, int], int]
+
+
+# prime numbers near to the 2**powers for power range of [1, 32]
+PRIMES = (
+    2, 3, 7, 17, 31, 67, 131, 257, 509, 1021, 2053, 4093, 8191, 16381, 32771, 65537, 131071, 262147, 524287, 1048573,
+    2097143, 4194301, 8388617, 16777213, 33554467, 67108859, 134217757, 268435459, 536870909, 1073741827, 2147483647,
+    4294967291
+
+
+)
+
+# Linear probing using prime size capacities.
+# Probing constants: probe: c1 = 1
+# For capacities above 2^32 it uses odd values based on the last prime.
+LINEAR_PROBER = Prober(
+    0.65,
+    lambda capacity, index: PRIMES[index + 3] if index + 3 < len(PRIMES) else capacity * 2 - 1,
+    lambda capacity, hash_, trie: (hash_ + trie) % capacity,
+)
+
+# Quadratic probing using prime size capacities.
+# However, due to probe the function, only half the indices are accessible.
+# Probing constants probe: c1 = c2 = 1; probe2: c1 = 0, c2 = 1; probe3: c1 = c2 = 1/2
+# For capacities above 2^32 it uses odd values based on the last prime (may fail).
+QUADRATIC_PRIME_PROBER = Prober(
+    0.5,
+    lambda capacity, index: PRIMES[index + 3] if index + 3 < len(PRIMES) else capacity * 2 - 1,
+    lambda capacity, hash_, trie: (hash_ + trie**2 + trie) % capacity,
+)
+# lambda capacity, hash_, trie: (hash_ + trie**2) % capacity,
+# lambda capacity, hash_, trie: (hash_ + (trie * 0.5)**2 + (trie * 0.5)) % capacity
+
+# Quadratic probing using triangular numbers that fully covers indices on a hashtable of capacity 2**n.
+# Probing constants for triangular numbers probe: c1 = c2 = 1/2
+# However, because of the even capacity, it may increase collisions among keys even and odd keys independently.
+QUADRATIC_TRIANGULAR_PROBER = Prober(
+    0.75,
+    lambda capacity, index: 2**(index + 3),
+    lambda capacity, hash_, trie: (hash_ + (trie**2 + trie) // 2) % capacity,
+)
