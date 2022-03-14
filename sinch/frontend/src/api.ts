20e8ff7312ae81f55e2f5abe94d1ae5fab662e00
Pedro Henrique <pedro00dk@gmail.com>
2022-03-14 16:01:04 -0300

    sinch: Temporary work on frontend

---
+++ sinch/frontend/src/api.ts
@@ -0,0 +1,5 @@
+export const API_ENDPOINT = import.meta.env['API_ENDPOINT']
+
+if (!API_ENDPOINT) throw new Error('API_ENDPOINT not set')
+
+export const api = (path: string) => `${API_ENDPOINT}${path}`
