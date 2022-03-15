3151b9c4065ac43cfc820155908991e44364d41c
Pedro Henrique <pedro00dk@gmail.com>
2022-03-15 16:21:24 -0300

    sinch: Partial work on frontend

--- sinch/frontend/src/api.ts
+++ sinch/frontend/src/api.ts
@@ -1,5 +1,5 @@
-export const API_ENDPOINT = import.meta.env['API_ENDPOINT']
+export const API_ENDPOINT = import.meta.env['VITE_API_ENDPOINT']
 
-if (!API_ENDPOINT) throw new Error('API_ENDPOINT not set')
+// if (!API_ENDPOINT) throw new Error('API_ENDPOINT not set')
 
 export const api = (path: string) => `${API_ENDPOINT}${path}`
