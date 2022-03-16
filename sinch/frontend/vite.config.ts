ae51acdc226531dd08c36deef7fb3c22e97c2c28
Pedro Henrique <pedro00dk@gmail.com>
2022-03-16 13:03:48 -0300

    sinch: Docker frontend setup done

--- sinch/frontend/vite.config.ts
+++ sinch/frontend/vite.config.ts
@@ -2,11 +2,15 @@ import pluginReactRefresh from '@vitejs/plugin-react-refresh'
 import pluginAnalyzer from 'rollup-plugin-analyzer'
 import { UserConfig } from 'vite'
 
+const SERVER_PORT = process.env.SERVER_PORT
+const PREVIEW_PORT = process.env.PREVIEW_PORT
+
 const config: UserConfig = {
     root: './src/',
     publicDir: './public/',
     build: { outDir: '../build/', minify: 'esbuild', target: 'es2021' },
-    server: { port: 3000 },
+    server: { port: SERVER_PORT ? +SERVER_PORT : undefined },
+    preview: { port: PREVIEW_PORT ? +PREVIEW_PORT : undefined },
     plugins: [pluginReactRefresh(), pluginAnalyzer()],
 }
 
