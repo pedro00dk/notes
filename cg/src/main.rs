5a1a7dc128593bbd9b120bacfde1c366158899d6
Pedro Henrique <pedro00dk@gmail.com>
2023-06-13 21:37:38 -0300

    cg: Tests with 2d canvas drawing

--- cg/src/main.rs
+++ cg/src/main.rs
@@ -1,21 +1,31 @@
 use leptos::*;
+// use js_sys::
+use wasm_bindgen::JsCast;
+use web_sys::CanvasRenderingContext2d;
 
 fn main() {
-    mount_to_body(|cx| view! { cx,  <App /> })
+    mount_to_body(|cx| view! { cx, <App /> })
 }
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, 0);
+    let canvas_ref = create_node_ref::<html::Canvas>(cx);
 
-    view! { cx,
-        <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1);
-            }
-        >
-            "Click me: "
-            {move || count.get()}
-        </button>
-    }
+    create_effect(cx, move |_| {
+        let canvas = canvas_ref.get();
+        if let None = canvas {
+            return;
+        }
+        let ctx = canvas
+            .unwrap()
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<CanvasRenderingContext2d>()
+            .unwrap();
+        ctx.fill_rect(10.0, 10.0, 100.0, 70.0);
+        log!("hello");
+    });
+
+    view! { cx, <canvas _ref=canvas_ref /> }
 }
