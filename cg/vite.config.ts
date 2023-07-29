bcdc66e7f6cfa8738c23ecb5bd3e0da9dbfdb3e0
Pedro Henrique <pedro00dk@gmail.com>
2023-07-29 13:47:37 -0300

    cg: Add compilation before server starts

--- cg/vite.config.ts
+++ cg/vite.config.ts
@@ -1,20 +1,16 @@
 import { execSync } from 'child_process'
 import type { Plugin, UserConfig } from 'vite'
 
-function wasmCompile(): Plugin {
-    return {
-        name: 'wasmCompile',
-        configureServer: async server => {
-            server.watcher.on('change', file => {
-                if (!file.endsWith('.rs')) return
-                console.log(file)
-                execSync(`npm run wasm:gen:${server.config.isProduction ? 'prd' : 'dev'}`, { stdio: 'inherit' })
-            })
-        },
-    }
-}
+const wasmCompilePlugin = (): Plugin => ({
+    name: 'wasmCompile',
+    configureServer: async ({ config: { isProduction }, watcher }) => {
+        const wasmCompile = () => execSync(`npm run wasm:gen:${isProduction ? 'prd' : 'dev'}`, { stdio: 'inherit' })
+        wasmCompile()
+        watcher.on('change', file => void (file.endsWith('.rs') && wasmCompile()))
+    },
+})
 
 export default {
     server: { port: 5000 },
-    plugins: [wasmCompile()],
+    plugins: [wasmCompilePlugin()],
 } as UserConfig
