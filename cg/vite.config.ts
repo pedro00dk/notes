a7dc1ac460a6c763f423ff359dd300dbc6266616
Pedro Henrique <pedro00dk@gmail.com>
2023-07-29 13:42:11 -0300

    cg: Create a vite plugin to automatically recompile wasm

--- cg/vite.config.ts
+++ cg/vite.config.ts
@@ -1,5 +1,20 @@
-import type { UserConfig } from 'vite'
+import { execSync } from 'child_process'
+import type { Plugin, UserConfig } from 'vite'
+
+function wasmCompile(): Plugin {
+    return {
+        name: 'wasmCompile',
+        configureServer: async server => {
+            server.watcher.on('change', file => {
+                if (!file.endsWith('.rs')) return
+                console.log(file)
+                execSync(`npm run wasm:gen:${server.config.isProduction ? 'prd' : 'dev'}`, { stdio: 'inherit' })
+            })
+        },
+    }
+}
 
 export default {
     server: { port: 5000 },
+    plugins: [wasmCompile()],
 } as UserConfig
