6a4e052f3f455d5b032f80eeff0109673aa6b8e2
Pedro Henrique <pedro00dk@gmail.com>
2023-08-06 00:32:14 -0300

    cg: Partial implementation of editor and player components

--- cg/src/components/editor.rs
+++ cg/src/components/editor.rs
@@ -1,26 +1,23 @@
-use js_sys::{Function, JsString, Object, Reflect};
+use js_sys::Function;
 use leptos::*;
 use wasm_bindgen::prelude::*;
 
-use crate::util::types::{editor, StandaloneCodeEditor};
+use crate::util::{js::js, types::monaco_editor};
 
 #[component]
 pub fn Editor<TOnChange>(
     cx: Scope,
     #[prop(optional)] language: &'static str,
     #[prop(optional)] theme: &'static str,
-    #[prop(optional)] set_editor: Option<WriteSignal<Option<StandaloneCodeEditor>>>,
+    #[prop(optional)] set_editor: Option<WriteSignal<Option<monaco_editor::StandaloneCodeEditor>>>,
     #[prop(default = None)] on_change: Option<TOnChange>,
 ) -> impl IntoView
 where
     TOnChange: Fn() + 'static,
 {
     let root = view! { cx, <div style="width: 100%; height: 100%" /> };
-    let options = Object::new();
-    Reflect::set(&options, &JsValue::from("automaticLayout"), &JsValue::TRUE).unwrap();
-    Reflect::set(&options, &JsValue::from("language"), &JsValue::from(language)).unwrap();
-    Reflect::set(&options, &JsValue::from("theme"), &JsValue::from(theme)).unwrap();
-    let code_editor = editor().create(&root, &options);
+    let options = js!({"automaticLayout": true, "language": language, "theme": theme});
+    let code_editor = monaco_editor::editor().create(&root, &options);
     if let Some(set_editor) = set_editor {
         set_editor.set(Some(code_editor.clone()));
     }
--- cg/src/components/player.rs
+++ cg/src/components/player.rs
@@ -1,5 +1,8 @@
-use crate::util::types;
-use js_sys::{Function, JsString, Object, Reflect};
+use crate::util::{
+    js::{js, js_fn},
+    types::feather_icons,
+};
+use js_sys::{Array, Function, JsString, Reflect};
 use leptos::{html, *};
 use wasm_bindgen::prelude::*;
 
@@ -15,40 +18,49 @@ pub fn Player(
 //     TOnChange: Fn() + 'static,
 {
     let canvas_ref = create_node_ref::<html::Canvas>(cx);
-    let player = view! {
+    let (resolution, set_resolution) = create_signal(cx, (0, 0));
+    let root = view! {
         cx,
-        <div>
+        <div class="components_player">
             <canvas _ref=canvas_ref />
             <div>
-                <button>  <svg
-                width="24"
-                height="24"
-                fill="none"
-                stroke="currentColor"
-            >
-                // <use_ href=types::sprite("circle").as_string() />
-            </svg></button>
-                <button>
-                <svg
-                    width="24"
-                    height="24"
-                    fill="none"
-                    stroke="currentColor"
-                >
-                    <use_ href="/node_modules/feather-icons/dist/feather-sprite.svg#circle"/>
-                </svg>
-                </button>
-                <span></span>
-                <span></span>
-                <span></span>
-                <button></button>
-                <button></button>
+                <PlayerButton icon="skip-back" />
+                <PlayerButton icon="play" />
+                <PlayerButton icon="pause" />
+                <span>140.3</span>
+                <span>60.1fps</span>
+                <span>{move || format!("{}x{}", resolution().0, resolution().1)}</span>
+                <PlayerButton icon="circle" color="marron" />
+                <PlayerButton icon="maximize" />
             </div>
         </div>
     };
+    web_sys::ResizeObserver::new(&js_fn!(<dyn Fn(Array)> move |entries: Array| {
+        let entry = js!(entries[0] as web_sys::ResizeObserverEntry).content_box_size().at(0);
+        let block = js!(entry["blockSize"]).as_f64().unwrap_or_default() as i32;
+        let inline = js!(entry["inlineSize"]).as_f64().unwrap_or_default() as i32;
+        set_resolution((block as i32, inline as i32));
+    }))
+    .unwrap()
+    .observe(&canvas_ref.get().unwrap());
+    root
+}
 
-    let canvas = canvas_ref.get().unwrap();
-    web_sys::console::log_1(&canvas);
-
-    player
+#[component]
+fn PlayerButton(
+    cx: Scope,
+    icon: &'static str,
+    #[prop(optional)] color: &'static str,
+    #[prop(optional)] record: bool,
+) -> impl IntoView {
+    // let color = if record { "maroon" } else { "black" };
+    let fill = if record { "maroon" } else { "none" };
+    let scale = if record { "maroon" } else { "none" };
+    view! { cx,
+        <button>
+            <svg viewBox="0 0 24 24" style:color=color>
+                <use_ href=feather_icons::name(icon) />
+            </svg>
+        </button>
+    }
 }
